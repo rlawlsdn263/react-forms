05d26365e4201bcadbed950635c1e63ae1250048
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 15:01:04 +0900

    [✍학습] useRef로 재렌더링 방지하기
    - useState를 사용하면 input에 값을 입력할 때마다 재렌더링이 발생함
    - useRef를 사용해 ref를 props로 전달해 사용하면 재렌더링을 막을 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import "./App.css";
-import FormInput from "./components/FormInput/FormInput";
-import { useState } from "react";
+import FormInput from "./components/FormInput/FormInput2";
+import { useRef } from "react";
 
 function App() {
-  const [username, setUsername] = useState("");
+  const usernameRef = useRef(null);
 
-  console.log(username);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(usernameRef);
+  }
+
   return (
     <div className="app">
-      <form>
-        <FormInput placeholder="Username" setUsername={setUsername}></FormInput>
+      <form onSubmit={handleSubmit}>
+        <FormInput usernameRef={usernameRef} placeholder="Username"></FormInput>
         <FormInput placeholder="Email"></FormInput>
         <FormInput placeholder="Full Name"></FormInput>
         <FormInput placeholder="Sth else"></FormInput>
+        <button>제출하기</button>
       </form>
     </div>
   );
--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -4,10 +4,7 @@ function FormInput(props) {
   return (
     <div className="formInput">
       {/* <label htmlFor="">Username</label> */}
-      <input
-        placeholder={props.placeholder}
-        onChange={(e) => props.setUsername(e.target.value)}
-      />
+      <input ref={props.usernameRef} placeholder={props.placeholder} />
     </div>
   );
 }
