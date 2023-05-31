2d709cbe168cffc21a914017005f01aa5810e5f8
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 17:46:41 +0900

    [🎨스타일] FormInput 스타일 완성

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import FormInput from "./components/FormInput/FormInput";
 import { useState } from "react";
 
 function App() {
-  // useState에 Form객체를 초깃값으로 제공함
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -12,7 +11,6 @@ function App() {
     confirmPassword: "",
   });
 
-  //리스트 렌더링을 위해 props가 담긴 배열을 만듦
   const inputs = [
     {
       id: 1,
@@ -55,9 +53,6 @@ function App() {
     e.preventDefault();
   }
 
-  //onChange 이벤트로 setValues()안에 전개 연산자로 이전의 객체값에서 변화된 값만 바꿈
-  //[e.target.name]을 사용해 useState 객체 안에 e.target.name과 일치하는 이름을 찾아 가져오고
-  //해당 state 데이터의 값만 e.target.value로 값을 변경함
   function handleChange(e) {
     setValues({ ...values, [e.target.name]: e.target.value });
   }
@@ -65,10 +60,7 @@ function App() {
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
-        {/* 
-          Array.map()으로 리스트 렌더링을 하고 받아온 props를 restProps로 풀어줌 
-          value 속성의 값으로 values 상태변수가 가진 값 중에 input.name과 겹치는 값에 접근함
-        */}
+        <h1>Register</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
