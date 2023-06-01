150a735d5b32a4e13fdbb4980261c1c0a54f98db
rlawlsdn263 <rlawlsdn263@naver.com>
2023-06-01 14:55:53 +0900

    [✍학습] onBlur를 사용한 유효성 검사기능
    - 사용자가 정규 표현식을 통과하지 않은 상태로 input을 변경할 경우 onBlur 이벤트핸들러가 focus 상태의 값을 바꿈

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./FormInput.css";
 
 function FormInput(props) {
+  const [focused, setFocused] = useState(false);
   const { label, errorMessage, onChange, ...restProps } = props;
+
+  function handleFocus() {
+    setFocused(true);
+  }
 
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...restProps} onChange={onChange} required />
+      <input
+        {...restProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
       <span>{errorMessage}</span>
     </div>
   );
